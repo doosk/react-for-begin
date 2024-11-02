8754e160e219b94263d1cdad2ba2c1af92043dfc
doosk <adm3@naver.com>
2024-11-02 11:33:42 +0900

    Initial

---
+++ src/App5.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+
+function App() {
+  console.log(process.env.PUBLIC_URL);
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movie/:id' element={<Detail />} />
+        <Route path='/hello' element={<h1>Hello</h1>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/routes/Detail.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState({});
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  console.log(movie);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1>Detail</h1>
+          <img src={movie.medium_cover_image} alt={movie.title} />
+          <h2>{movie.title}</h2>
+        </div>
+      )}
+    </div>
+  );
+}
+export default Detail;
